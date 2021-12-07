04af3e511dab5fd6c50b075029fa43c569969e73
defne <defne.keskin@etud.univ-jfc.fr>
2021-12-07 14:25:37 +0100

    Test 100%

--- src/main/java/FFSSM/Club.java
+++ src/main/java/FFSSM/Club.java
@@ -3,12 +3,13 @@
  */
 package FFSSM;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Club {
 
- 
     public Moniteur president;
 
     public String nom;
@@ -17,33 +18,45 @@ public class Club {
 
     public String telephone;
 
+    public ArrayList<Plongee> plonges;
+
     public Club(Moniteur président, String nom, String telephone) {
+        plonges = new ArrayList<Plongee>();
         this.president = président;
         this.nom = nom;
         this.telephone = telephone;
     }
 
     /**
-     * Calcule l'ensemble des plongées non conformes organisées par ce club.
-     * Une plongée est conforme si tous les plongeurs de la palanquée ont une licence
-     * valide à la date de la plongée
+     * Calcule l'ensemble des plongées non conformes organisées par ce club. Une
+     * plongée est conforme si tous les plongeurs de la palanquée ont une
+     * licence valide à la date de la plongée
+     *
      * @return l'ensemble des plongées non conformes
      */
     public Set<Plongee> plongeesNonConformes() {
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        Set<Plongee> plongesConforme = new HashSet<Plongee>();
+        for (Plongee p : plonges) {
+
+            if (p.estConforme() == false) {
+                plongesConforme.add(p);
+            }
+        }
+        return plongesConforme;
+
     }
+
+  
 
     /**
      * Enregistre une nouvelle plongée organisée par ce club
+     *
      * @param p la nouvelle plongée
      */
     public void organisePlongee(Plongee p) {
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+        plonges.add(p);    
     }
-    
-    
+
     public Moniteur getPresident() {
         return president;
     }
